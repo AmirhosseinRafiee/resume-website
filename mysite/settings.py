146631c194ffac215eba059801276cfffaa479d0
amirhr21 <amirhossein.rafiee.80@gmail.com>
2022-03-10 00:41:46 +0330

    updated static_root

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
-MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'statics',
