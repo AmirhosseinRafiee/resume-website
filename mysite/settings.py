9b27c0579fafcdd7c2d92a54d4e48f5eedc52146
amirhr21 <amirhossein.rafiee.80@gmail.com>
2022-03-10 00:30:01 +0330

    turned off debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bm491#8z_(ddq@00f687w2brt9e9r$^xs!fp4%nsfe1u0ye3q_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
