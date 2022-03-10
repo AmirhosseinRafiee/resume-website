ff70ab63cdb6422400867d322ba6ccc4226ed637
amirhr21 <amirhossein.rafiee.80@gmail.com>
2022-03-10 21:42:40 +0330

    added url to allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bm491#8z_(ddq@00f687w2brt9e9r$^xs!fp4%nsfe1u0ye3q_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://amirhosseinrafiee.herokuapp.com/']
 
 
 # Application definition
